d74027d5a222fff286a03baa515fe0b856f271f5
taina Adrielly <martins.taina@aluno.ifsp.edu.br>
2026-03-09 08:30:57 -0300

    09/03

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mustache from 'mustache-express';
 import path from 'path';
+import mainRouter from './routes/index';
 
 dotenv.config();
 
@@ -12,5 +13,10 @@ server.set('views', path.join(__dirname, 'views'));
 server.engine('mustache', mustache());
 
 server.use(express.static(path.join(__dirname, '../public')));
+
+server.use(mainRouter);
+server.use((req, res)=> {
+    res.send('404 - Not Found');
+})
 
 server.listen(process.env.PORT);
