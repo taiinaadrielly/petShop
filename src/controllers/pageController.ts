9fbe6bfe37774adf32b8de0928021c6865ba9a4b
taina adrielly <martins.taina@aluno.ifsp.edu.br>
2026-03-16 08:34:32 -0300

    views

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -7,20 +7,35 @@ export const home = (req: Request, res: Response) => {
             title: 'Todos os animais',
             background:'allanimals.jpg'
         }
-    })
-}
+    });
+};
 
 export const dogs = (req: Request, res: Response) => {
     //res.send('dogs');
-    res.render('pages/page')
-}
+    res.render('pages/page', {
+         banner:{
+            title: 'Cachorro',
+            background:'banner_dog.jpg'
+        }
+    });
+};
 
 export const cats = (req: Request, res: Response) => {
     //res.send('cats');
-    res.render('pages/page')
-}
+    res.render('pages/page', {
+         banner:{
+            title: 'Gatos',
+            background:'banner_cat.jpg'
+        }
+    });
+};
 
 export const fishes = (req: Request, res: Response) => {
     //res.send('fishes');
-    res.render('pages/page')
+    res.render('pages/page' , {
+         banner:{
+            title: 'Peixes',
+            background:'banner_fish.jpg'
+        }
+    })
 }
